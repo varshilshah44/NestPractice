5345bb43ad82a13641c0c7394271b9fb3cef2df0
varshilshah44 <varshil.shah@solulab.co>
2023-04-18 09:45:43 +0530

    migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity';
+// import * as ormConfig from '../ormconfig.js';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
@@ -23,6 +24,7 @@ const cookieSession = require('cookie-session');
       entities: [User, Report],
       synchronize: true,
     }), */
+
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
@@ -34,6 +36,7 @@ const cookieSession = require('cookie-session');
         };
       },
     }),
+    // TypeOrmModule.forRoot(ormConfig),
     UsersModule,
     ReportsModule,
   ],
